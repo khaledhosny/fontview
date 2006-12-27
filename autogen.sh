f4b12966a618f8aa22ece3813849b25b62b30eb2
bse <bse@eab3d5f5-2e2e-0410-998a-5544d110140b>
2006-12-27 02:42:22 +0000

    Rearrangement.
    git-svn-id: http://serif.googlecode.com/svn/fv@13 eab3d5f5-2e2e-0410-998a-5544d110140b

---
+++ autogen.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# autogen.sh from Valgrind
+
+run ()
+{
+    echo "running: $*"
+    eval $*
+
+    if test $? != 0 ; then
+	echo "error: while running '$*'"
+	exit 1
+    fi
+}
+
+run aclocal
+run autoheader
+run automake --add-missing
+run autoconf
