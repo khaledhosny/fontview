37f1e80a99f6ce1a97485eec574928669e101762
Khaled Hosny <khaledhosny@eglug.org>
2013-09-17 17:48:42 +0200

    Revert "Remove unused functions"
    This reverts commit 7aef7f2b2842cda7c8fe86429ea78f1f30d181a5.

--- src/font-view.c
+++ src/font-view.c
@@ -104,6 +104,10 @@ static void font_view_init (FontView *view) {
             GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK);
 }
 
+GtkWidget *font_view_new () {
+    return g_object_new (FONT_VIEW_TYPE, NULL);
+}
+
 GtkWidget *font_view_new_with_model (gchar *font) {
     FontView *view;
     FontViewPrivate *priv;
@@ -121,6 +125,15 @@ GtkWidget *font_view_new_with_model (gchar *font) {
     }
 
     return GTK_WIDGET(view);
+}
+
+void font_view_set_model (FontView *view, FontModel *model) {
+    FontViewPrivate *priv;
+    priv = FONT_VIEW_GET_PRIVATE(view);
+
+    if (IS_FONT_MODEL(model)) {
+        priv->model = model;
+    }
 }
 
 FontModel *font_view_get_model (FontView *view) {
@@ -318,6 +331,13 @@ static void font_view_redraw (FontView *view) {
     cairo_region_destroy (region);
 }
 
+gdouble font_view_get_pt_size (FontView *view) {
+    FontViewPrivate *priv;
+
+    priv = FONT_VIEW_GET_PRIVATE (view);
+    return priv->size;
+}
+
 void font_view_set_pt_size (FontView *view, gdouble size) {
     FontViewPrivate *priv;
 
@@ -333,13 +353,6 @@ void font_view_set_pt_size (FontView *view, gdouble size) {
     priv->extents[TEXT] = TRUE;
 
     font_view_redraw (view);
-}
-
-gdouble font_view_get_pt_size (FontView *view) {
-    FontViewPrivate *priv;
-
-    priv = FONT_VIEW_GET_PRIVATE (view);
-    return priv->size;
 }
 
 gchar *font_view_get_text (FontView *view) {
