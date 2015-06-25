0a9e0364ec7401f33546b6a90e2a24509bcad18e
Khaled Hosny <khaledhosny@eglug.org>
2015-06-25 13:26:32 +0200

    Simplify

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,2 @@
 #!/bin/sh
-
-run ()
-{
-    echo "running: $*"
-    eval $*
-
-    if test $? != 0 ; then
-	echo "error: while running '$*'"
-	exit 1
-    fi
-}
-
-run autoreconf --install --force
+autoreconf --install --force
